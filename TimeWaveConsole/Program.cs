593c0e26dae6327b1420f308ee429229f5645ad2
Charles Wuthrich <chuck.wuthrich@outlook.com>
2019-12-16 19:15:31 -0500

    Added a console application to test the class library.

---
+++ TimeWaveConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using timewave_z3r0_dotNET;
+
+namespace TimeWaveConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string[] retArray = timewave_z3r0_dotNET.DATA.ReadTWData.GetTimeWaveData();
+
+            for (int i = 0; i < retArray.Length; i++)
+            {
+                
+            }
+        }
+    }
+}
---
+++ timewave_z3r0_dotNET/DATA/ReadTWData.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace timewave_z3r0_dotNET.DATA
+{
+    public class ReadTWData
+    {
+        public static string[] GetTimeWaveData()
+        {
+            return
+                File.ReadAllLines("C:\\Users\\chuck\\source\\repos\\timewave_z3r0\\timewave_z3r0_dotNet\\DATA\\DATA.TW1");
+        }
+    }
+}
